900d7e98a4028a53163fcf39d21056d6c690e377
hfiga <hfigaorozco@gmail.com>
2026-06-17 23:46:00 -0700

    Primera version de modelos corregidos

--- osat_tracer/api_maquinaria/models.py
+++ osat_tracer/api_maquinaria/models.py
@@ -1,33 +1,41 @@
 from django.db import models
+from api_usuarios.models import Empleado
+from api_produccion.models import Linea
 
 # Create your models here.
-class Maquina(models.Model):
-    numSerie = models.CharField(default="ABCDE", max_length=5)
-    fechaReg = models.TimeField(auto_now=False, auto_now_add=True)
-    tipoMaquina = models.CharField(default="", max_length=5)
-    estado = models.CharField(default="", max_length=5)
-    empleado = models.IntegerField(default=0)
-    linea = models.CharField(default="", max_length=5)
-    
-    class Meta: 
-        db_table = 'maquina'
-    def __str__(self):
-        return self.numSerie
-
 class Tipo_Maquina(models.Model):
-    clave = models.CharField(default="", max_length=5)
+    clave = models.CharField(primary_key=True, max_length=5)
     descripcion = models.CharField(default="", max_length=30)
     
     class Meta: 
         db_table = 'tipo_maquina'
+        
     def __str__(self):
         return self.descripcion
     
+    
 class Estado_Maquina(models.Model):
-    clave = models.CharField(default="", max_length=5)
-    descripcion = models.CharField(default="", max_length=15)
+    clave = models.CharField(primary_key=True, max_length=5)
+    descripcion = models.CharField(default="Estado genérico", unique=True, max_length=15)
     
     class Meta: 
         db_table = 'estado_maquina'
+        
     def __str__(self):
         return self.descripcion
+    
+
+class Maquina(models.Model):
+    numSerie = models.CharField(primary_key=True, max_length=5)
+    nombre = models.CharField(default="Máquina genérica", unique=True, max_length=30)
+    fechaReg = models.TimeField(auto_now=False, auto_now_add=True)
+    tipoMaquina = models.ForeignKey(Tipo_Maquina, on_delete=models.CASCADE, related_name='maquina')
+    estado = models.ForeignKey(Estado_Maquina, on_delete=models.CASCADE, related_name='maquina')
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE, related_name='maquina')
+    linea = models.ForeignKey(Linea, on_delete=models.CASCADE, related_name='maquina')
+    
+    class Meta: 
+        db_table = 'maquina'
+        
+    def __str__(self):
+        return self.numSerie
--- osat_tracer/api_usuarios/models.py
+++ osat_tracer/api_usuarios/models.py
@@ -1,30 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Empleado(models.Model):
-    numero = models.AutoField(primary_key=True)
-    nombre = models.CharField(default=(""), max_length=40)
-    primerApell = models.CharField(default=(""), max_length=40)
-    seguApell = models.CharField(default=(""), max_length=40)
-    rfc = models.CharField(default=(""), max_length=13)
-    email = models.CharField(default=(""), max_length=50)
-    fechaReg = models.DateField(auto_now=False, auto_now_add=True)
-    estado = models.CharField(default=(""), max_length=5)
-    rol = models.CharField(default=(""), max_length=5)    
-    
-    def __str__(self):
-        return self.nombre
-    
 class Rol(models.Model):
-    codigo = models.CharField(default="", max_length=5)
-    descripcion = models.CharField(default="", max_length=20)
+    codigo = models.CharField(primary_key=True, max_length=5)
+    descripcion = models.CharField(default="Rol genérico", unique=True, max_length=20)
+    
+    class Meta:
+        db_table = 'rol'
     
     def __str__(self):
         return self.descripcion
 
+
 class Estado_Empleado(models.Model):
-    codigo = models.CharField(default="", max_length=5)
-    descripcion = models.CharField(default="", max_length=15)
+    codigo = models.CharField(primary_key=True, max_length=5)
+    descripcion = models.CharField(default="Estado de empleado genérico", unique=True, max_length=15)
+    
+    class Meta:
+        db_table = 'estado_empleado'
     
     def __str__(self):
         return self.descripcion
+    
+
+class Empleado(models.Model):
+    numero = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+    primerApell = models.CharField(max_length=40)
+    seguApell = models.CharField(max_length=40)
+    rfc = models.CharField(unique=True, max_length=13)
+    email = models.EmailField(unique=True, max_length=50)
+    fechaReg = models.DateField(auto_now_add=True)
+    estado = models.ForeignKey(Estado_Empleado, on_delete=models.CASCADE, related_name='empleado')
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE, related_name='empleado')
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='empleado')
+
+    class Meta:
+        db_table = 'empleado'
+
+    def __str__(self):
+        return f"{self.nombre} {self.primerApell}"
