c85f9c3400cfc775b1ec016547df4ecf8e32cb22
Jose Flores <floresruizj53@gmail.com>
2026-06-23 15:02:40 -0700

    adminSerialiazer api_produccion

--- osat_tracer/api_produccion/admin.py
+++ osat_tracer/api_produccion/admin.py
@@ -1,9 +1,131 @@
 from django.contrib import admin
 from api_produccion import models
-@admin.register(models.Estado_Orden)
-# Register your models here.
-class BankAdmin(admin.ModelAdmin):
+    
+@admin.register(models.Defecto)
+class DefectoAdmin(admin.ModelAdmin):
     list_display= [
         "codigo",
         "descripcion",
     ]
+    
+@admin.register(models.Tipo_Oblea)
+class TipoObleaAdmin(admin.ModelAdmin):
+    list_display= [
+        "codigo",
+        "descripcion",
+        "cantidadDies"
+    ]
+
+@admin.register(models.Estado_Paso)
+class EstadoPasoAdmin(admin.ModelAdmin):
+    list_display= [
+        "codigo",
+        "descripcion",
+    ]
+
+@admin.register(models.Estado_Orden)
+class EstadoOrdenAdmin(admin.ModelAdmin):
+    list_display= [
+        "codigo",
+        "descripcion",
+    ]
+
+@admin.register(models.Estado_Oblea)
+class EstadoObleaAdmin(admin.ModelAdmin):
+    list_display= [
+        "codigo",
+        "descripcion",
+    ] 
+    
+@admin.register(models.Linea)
+class LineaAdmin(admin.ModelAdmin):
+    list_display= [
+        "codigo",
+        "nombre",
+    ] 
+    
+@admin.register(models.Proceso)
+class ProcesoAdmin(admin.ModelAdmin):
+    list_display= [
+        "codigo",
+        "nombre",
+        "descripcion",
+        "imagen",
+    ]
+
+@admin.register(models.Paso)
+class PasoAdmin(admin.ModelAdmin):
+    list_display= [
+        "codigo",
+        "nombre",
+        "descripcion",
+        "tiempoEstimado",
+    ]  
+    
+@admin.register(models.Orden)
+class OrdenAdmin(admin.ModelAdmin):
+    list_display= [
+        "horaIni",
+        "horaFin",
+        "fechaReg",
+        "proceso",
+        "estado",
+        "empleado",
+    ]  
+
+@admin.register(models.Oblea)
+class ObleaAdmin(admin.ModelAdmin):
+    list_display= [
+        "diesGeneados",
+        "orden",
+        "estado",
+        "tipo",        
+    ]  
+    
+@admin.register(models.LineaProceso)
+class LineaProcesoAdmin(admin.ModelAdmin):
+    list_display= [
+        "linea",
+        "proceso",      
+    ]  
+
+@admin.register(models.PasoProceso)
+class PasoProcesoAdmin(admin.ModelAdmin):
+    list_display= [
+        "paso",
+        "proceso",
+        "orden",      
+    ]  
+
+@admin.register(models.ProcesoPieza)
+class ProcesoPiezaAdmin(admin.ModelAdmin):
+    list_display= [
+        "proceso",
+        "pieza",
+        "cantPieza",      
+    ]  
+
+@admin.register(models.MaquinaPaso)
+class MaquinaPasoAdmin(admin.ModelAdmin):
+    list_display= [
+        "maquina",
+        "paso",    
+    ]  
+
+@admin.register(models.Paso_Realizado)
+class PasoRealizadoAdmin(admin.ModelAdmin):
+    list_display= [
+        "hora",
+        "fecha",
+        "paso",
+        "estado",
+        "oblea",
+        "alerte",    
+    ]  
+    
+@admin.register(models.Historial_Defectos)
+class HistorialDefectosAdmin(admin.ModelAdmin):
+    list_display= [
+        "defecto",
+        "pasoRealizado",
+    ]  
