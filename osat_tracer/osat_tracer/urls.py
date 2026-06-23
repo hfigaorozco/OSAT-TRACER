68d1aa226b50e283679c3bd32dac32d26456a1d9
hfiga <hfigaorozco@gmail.com>
2026-06-23 15:58:28 -0700

    volviendo urls

--- osat_tracer/osat_tracer/urls.py
+++ osat_tracer/osat_tracer/urls.py
@@ -19,4 +19,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api_inventario.urls')),
+    #path('api/', include('api_kpi.urls')),
+    #path('api/', include('api_maquinaria.urls')),
+    #path('api/', include('api_produccion.urls')),
+    #path('api/', include('api_reportes.urls')),
+    #path('api/', include('api_usuarios.urls'))
 ]
