3e5257013052798634df407d181d2a24005e0da3
hfiga <hfigaorozco@gmail.com>
2026-06-23 16:05:13 -0700

    Arreglando urls que no jalan

--- osat_tracer/api_inventario/urls.py
+++ osat_tracer/api_inventario/urls.py
@@ -2,13 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('v1/list/piezas', views.ListPiezaAPIView.as_view(), name='list_piezas'),
-    path('v1/create/pieza', views.CreatePiezaAPIView.as_view(), name='create_pieza'),
-    path('v1/detail/pieza/<str:pk>', views.CreatePiezaAPIView.as_view(), name='create_pieza'),
-=======
     path('v1/list/piezas/', views.ListPiezaAPIView.as_view(), name='list_piezas'),
     path('v1/create/pieza/', views.CreatePiezaAPIView.as_view(), name='create_pieza'),
     path('v1/detail/pieza/<str:pk>/', views.CreatePiezaAPIView.as_view(), name='detail_pieza'),
->>>>>>> f7ca8c94e196b8c15f7543c16d6d50ab2fab240a
 ]
