368530b861463e5a346ab05080a164143563108a
hfiga <hfigaorozco@gmail.com>
2026-06-23 17:36:56 -0700

    APIs y URLs de usuarios hechas

--- osat_tracer/api_inventario/urls.py
+++ osat_tracer/api_inventario/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('v1/list/piezas/', views.ListPiezaAPIView.as_view(), name='list_piezas'),
     path('v1/create/pieza/', views.CreatePiezaAPIView.as_view(), name='create_pieza'),
-    path('v1/detail/pieza/<str:pk>/', views.CreatePiezaAPIView.as_view(), name='detail_pieza'),
+    path('v1/detail/pieza/<str:pk>/', views.DetailPiezaAPIView.as_view(), name='detail_pieza'),
 ]
--- osat_tracer/api_usuarios/views.py
+++ osat_tracer/api_usuarios/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import models, serializers
 
-# Create your views here.
+### CRUD EMPLEADOS
+
+## List
+class ListEmpleadoAPIView(generics.ListAPIView):
+    queryset = models.Empleado.objects.all()
+    serializer_class = serializers.EmpleadoListSerializer
+
+
+## Detail
+class DetailEmpleadoAPIView(generics.RetrieveAPIView):
+    queryset = models.Empleado.objects.all()
+    serializer_class = serializers.EmpleadoListSerializer
+    lookup_field = 'numero'
+
+
+## Create
+class CreateEmpleadoAPIView(generics.CreateAPIView):
+    serializer_class = serializers.EmpleadoCreateSerializer
+
+
+## Update
+class UpdateEmpleadoAPIView(generics.UpdateAPIView):
+    queryset = models.Empleado.objects.all()
+    serializer_class = serializers.EmpleadoUpdateSerializer
+    lookup_field = 'numero'
