b4da7f84d8c67601a57ac8f40640fccef941f795
Aartwo0 <ajjdlr2003@gmail.com>
2026-08-06 08:33:26 -0700

    merge

--- osat_tracer/osat_tracer/settings.py
+++ osat_tracer/osat_tracer/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2','192.168.1.68']
 
 
 # Application definition
-
+    
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
